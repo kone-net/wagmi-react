56a846a03dae687d0e0dcd4a47b01709a9cc6195
kone-net <kone_net@163.com>
2024-05-27 23:14:11 +0800

    feat: gwei convert to ETH

--- src/wallet/Account.tsx
+++ src/wallet/Account.tsx
@@ -7,6 +7,11 @@ export function Account() {
   const { disconnect } = useDisconnect()
   const balanceValue = useBalance({ address })
   console.log(balanceValue)
+
+  var value: string = "0"
+  if (balanceValue?.data?.value) {
+    value = (Number(balanceValue?.data?.value) /  Number(1e18)).toFixed(4)
+}
   const { data: ensName } = useEnsName({ address })
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! })
 
@@ -14,13 +19,13 @@ export function Account() {
     <div>
       {isConnected && (
         <div>
-          当前钱包地址: {address}<br />
-          余额: {balanceValue.data?.value.toString()} <br/>
+          当前钱包地址: {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
+          {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}<br />
+          余额: {balanceValue.data?.value.toString()} gwei <br/>
+          余额(ETH): {value.toString()} {balanceValue?.data?.symbol}<br/>
         </div>
       )}
-      {balanceValue.isSuccess && <div>{balanceValue.data.symbol}</div>}
-      {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
-      {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
+      
       <button onClick={() => disconnect()}>Disconnect</button>
 
       <SendTransaction />
